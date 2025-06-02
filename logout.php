e17d530a65d77190baed0f887151d4837d5c6a3b
Carlos Eduardo <130923575+Cadu21@users.noreply.github.com>
2025-06-02 04:30:08 -0300

    Delete logout.php

--- logout.php
+++
@@ -1,8 +0,0 @@
-<?php require_once(__DIR__ . '/../includes/verifica_instalacao.php');
-
-session_start();
-session_unset();  // Limpa as variáveis de sessão
-session_destroy(); // Destrói a sessão
-
-header("Location: ../views/login.html"); // Redireciona para o login
-exit();
